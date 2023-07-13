b0e103554cc3ba6721c25728086c27a2cf7a84c0
Шибаев Алексей Евгеньевич (4094442) <VTB4094442@region.vtb.ru>
2023-07-13 21:22:44 +0300

    init

---
+++ src/main/java/com/company/app/core/telegram/domain/entity_graph/ChatEntityGraph.java
@@ -0,0 +1,9 @@
+package com.company.app.core.telegram.domain.entity_graph;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+
+@DynamicEntityGraph
+public interface ChatEntityGraph<Chat, Long> {
+
+
+}
